443fcc44b6664b9b00d10d0d874bd8fe7795d200
zhonghaoliu <joshualiu94@gmail.com>
2018-12-05 12:42:11 -0500

    first component

---
+++ src/components/StorePicker.js
@@ -0,0 +1,17 @@
+import React, { Component, Fragment } from "react";
+
+class StorePicker extends Component {
+  render() {
+    return (
+      <Fragment>
+        <form className="store-selector">
+          <h2>Please Enter A Store</h2>
+          <input type="text" required placeholder="Store Name"/>
+          <button type="submit">Visit Store →</button>
+        </form> 
+      </Fragment>
+    );
+  }
+}
+
+export default StorePicker;
