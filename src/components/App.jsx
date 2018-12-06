1f8192d40f66e28b1e300129129f47ab357e6220
zhonghaoliu <joshualiu94@gmail.com>
2018-12-06 14:04:06 -0500

    create fish list, pass data down to the Fish component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,6 +3,8 @@ import Header from "./Header";
 import Inventory from "./Inventory";
 import Order from "./Order";
 import sampleFishes from "../sample-fishes";
+import Fish from "./Fish";
+
 class App extends Component {
   constructor() {
     super();
@@ -20,7 +22,7 @@ class App extends Component {
   };
 
   loadSampleFishes = () => {
-    this.setState({fishes: sampleFishes})
+    this.setState({ fishes: sampleFishes });
   };
 
   render() {
@@ -28,6 +30,11 @@ class App extends Component {
       <div className="catch-of-the-day">
         <div className="menu">
           <Header tagline="Fresh Seafood Market" />
+          <ul className="fishes">
+            {Object.keys(this.state.fishes).map(key => (
+              <Fish key={key} details={this.state.fishes[key]} />
+            ))}
+          </ul>
         </div>
         <Order />
         <Inventory
