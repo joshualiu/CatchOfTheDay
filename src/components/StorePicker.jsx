771ab6c1c4dad4e54ea5de3b76de25b753cf6902
zhonghaoliu <joshualiu94@gmail.com>
2018-12-06 11:36:08 -0500

    handling events

--- src/components/StorePicker.jsx
+++ src/components/StorePicker.jsx
@@ -1,14 +1,33 @@
 import React, { Component, Fragment } from "react";
+import { getFunName } from "../helpers"; // generate a random fun store name
 
 class StorePicker extends Component {
+  constructor() {
+    super();
+    // this.goToStore = this.goToStore.bind(this)
+  }
+  myInput = React.createRef();
+
+  goToStore = (event) => {
+    event.preventDefault();    // stop the form from submitting
+    const storeName = this.myInput.value.value;
+    this.props.history.push(`/store/${this.storeName}`)
+  }
+
   render() {
     return (
       <Fragment>
-        <form className="store-selector">
+        <form className="store-selector" onSubmit={this.goToStore}>
           <h2>Please Enter A Store</h2>
-          <input type="text" required placeholder="Store Name"/>
+          <input
+            type="text"
+            ref={this.myInput}
+            required
+            placeholder="Store Name"
+            defaultValue={getFunName()}
+          />
           <button type="submit">Visit Store →</button>
-        </form> 
+        </form>
       </Fragment>
     );
   }
