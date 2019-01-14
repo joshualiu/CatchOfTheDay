c94c383c6bb03941aefa43c919dd03f526958537
zhonghaoliu <joshualiu94@gmail.com>
2019-01-14 14:17:53 -0500

    update secret credentials

--- src/base.js
+++ src/base.js
@@ -1,11 +1,18 @@
 import Rebase from "re-base";
 import firebase from "firebase";
-import config from "./secret";
+require('dotenv').config();
+// import config from "./secret";
 
-const firebaseApp = firebase.initializeApp(config);
+const firebaseApp = firebase.initializeApp({
+  apiKey: process.env.REACT_APP_APIKEY,
+  authDomain: process.env.REACT_APP_AUTHDOMAIN,
+  databaseURL: process.env.REACT_APP_DATABASEURL
+});
 
 const base = Rebase.createClass(firebaseApp.database());
 
 export { firebaseApp };
 
 export default base;
+
+
